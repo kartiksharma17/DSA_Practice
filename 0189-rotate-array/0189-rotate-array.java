9d7fb769c18de3913dbaba9f74f07b1a8948480e
damnyoukartik <121182425+kartiksharma17@users.noreply.github.com>
2026-07-10 23:32:28 +0530

    Time: 2 ms (19.83%), Space: 61.2 MB (55.04%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,30 +1,85 @@
 class Solution {
     public void rotate(int[] nums, int k) {
 
-        ArrayList<Integer> temp = new ArrayList<>();
+        //this is brute force 
+
+        // ArrayList<Integer> temp = new ArrayList<>();
+        // int n = nums.length;
+        // k = k % n; 
+
+        // //element storing from n-1 to k
+        // int i =n-1;
+        // while(i>=n-k){
+        //     temp.add(nums[i]);
+        //     i--;
+        // }
+
+        // //right shifting remaining elements 
+        //     for (int j = n - k - 1; j >= 0; j--) {
+        //     nums[j + k] = nums[j];
+        // }
+
+        // //placing the temp array element 
+        // //eg [7,6,5]
+        // for(int l=0; l<temp.size(); l++){
+
+        //     nums[(k-1)-l] =temp.get(l);
+
+
+        // }
+
+
+        //trying to optimize it
+
         int n = nums.length;
-        k = k % n; 
+        k=k%n;
 
-        //element storing from n-1 to k
-        int i =n-1;
-        while(i>=n-k){
-            temp.add(nums[i]);
-            i--;
+        
+
+        int right=n-1;
+        int left=n-k;
+        while(right>left){
+            int temp = nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+
+            right--;
+            left++;  
+            
         }
 
-        //right shifting remaining elements 
-            for (int j = n - k - 1; j >= 0; j--) {
-            nums[j + k] = nums[j];
+        left=0;
+        right =n-k-1;
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+
+            right--;
+            left++;
         }
 
-        //placing the temp array element 
-        //eg [7,6,5]
-        for(int l=0; l<temp.size(); l++){
+        //reversing whole array now
+        left= 0;
+        right=n-1;
 
-            nums[(k-1)-l] =temp.get(l);
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
 
-
+            right--;
+            left++;
         }
+
+        
+
+
+
+
+
+
+
         
     }
 }
