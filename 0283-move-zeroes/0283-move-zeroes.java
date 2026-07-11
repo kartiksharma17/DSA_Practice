feb96eb04ac9febf7fdb72d645238498bb1fbf54
damnyoukartik <121182425+kartiksharma17@users.noreply.github.com>
2026-07-11 17:31:21 +0530

    Time: 2 ms (92.23%), Space: 47.8 MB (38.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,44 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        ArrayList<Integer> temp= new ArrayList<>(); 
-        for(int i =0; i<n; i++){
-            if(nums[i]!=0){
-                temp.add(nums[i]);
+
+        // //brute force
+        // int n=nums.length;
+        // ArrayList<Integer> temp= new ArrayList<>(); 
+        // for(int i =0; i<n; i++){
+        //     if(nums[i]!=0){
+        //         temp.add(nums[i]);
+
+        //     }
+            
+        // }
+        // for(int j=0;j<temp.size();j++){
+        //     nums[j] = temp.get(j);
+        // }
+
+        // for(int k=temp.size(); k<n; k++){
+        //     nums[k] = 0;
+        // }
+
+
+        //optimal solution
+
+        int n= nums.length;
+        int low =0;
+        
+        
+        for(int high=0; high<n; high++){
+            if(nums[high]!=0){
+                int temp =nums[low];
+                nums[low]=nums[high];
+                nums[high]=temp;
+                low++;
+
 
             }
             
         }
-        for(int j=0;j<temp.size();j++){
-            nums[j] = temp.get(j);
-        }
 
-        for(int k=temp.size(); k<n; k++){
-            nums[k] = 0;
-        }
+       
 
 
     }
