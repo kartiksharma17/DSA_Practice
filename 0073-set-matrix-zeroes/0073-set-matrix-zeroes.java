295674e81a113124e7523c05d3dd0195256fff51
damnyoukartik <121182425+kartiksharma17@users.noreply.github.com>
2026-07-12 14:37:18 +0530

    Time: 1 ms (98.61%), Space: 47.7 MB (43.61%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,7 +1,7 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
 
-        //brute
+/*     //brute
 
         boolean[][] mark = new boolean[matrix.length][matrix[0].length];
 
@@ -40,4 +40,35 @@ class Solution {
             }
         }
     }
+*/
+
+
+        //better
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int row[]= new int[n];
+
+        int col[]= new int[m];
+
+        for(int i=0; i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0; j<m;j++ ){
+                if(row[i]==1 || col[j]==1){
+                    matrix[i][j]=0;
+                }
+            }
+            
+        }
+        
+        
+    }
+    
 }
