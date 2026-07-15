82f15014287a30206f30c2e64f028cdfba8ea7e3
damnyoukartik <121182425+kartiksharma17@users.noreply.github.com>
2026-07-15 22:34:10 +0530

    Time: 13 ms (31.25%), Space: 52.4 MB (90.84%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,24 +2,47 @@ class Solution {
     public int majorityElement(int[] nums) {
         //brute
         
-        int len=nums.length;
+        // int len=nums.length;
         
-        for(int i=0; i<len; i++){
-            int count=0;
-            for(int j=0; j<len; j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
+        // for(int i=0; i<len; i++){
+        //     int count=0;
+        //     for(int j=0; j<len; j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //         }
+        //     }
            
-            if(count>len/2){
-                return nums[i];
-            }
+        //     if(count>len/2){
+        //         return nums[i];
+        //     }
 
             
+        // }
+        
+        // return -1;
+
+
+
+        //better 
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int len=nums.length;
+
+
+        //counting frequency 
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int target=len/2;
+
+        //finding majority element
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue()>target){
+                return entry.getKey();
+            }
+
         }
         
         return -1;
-        
     }
 }
