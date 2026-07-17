35cb7b9908b0f82e174d1985c36a3d258f5e7452
damnyoukartik <121182425+kartiksharma17@users.noreply.github.com>
2026-07-17 17:14:42 +0530

    Time: 3 ms (100%), Space: 175.7 MB (15.57%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,33 +1,55 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         //brute force
+        // int n=nums.length;
+        // int pos[]=new int[n/2];
+        // int neg[]=new int[n/2];
+
+        // int posIdx=0;
+        // int negIdx=0;
+
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>=0){
+        //         pos[posIdx]=nums[i];
+        //         posIdx++;
+
+        //     }
+        //     else{
+        //         neg[negIdx]=nums[i];
+        //         negIdx++;
+
+        //     } 
+        // }
+
+        // for(int j=0;j<n/2;j++){
+        //     nums[2*j]=pos[j];
+        //     nums[2*j+1]=neg[j];
+
+        // }
+
+        // return nums;
+
+
+
+        //optimized
         int n=nums.length;
-        int pos[]=new int[n/2];
-        int neg[]=new int[n/2];
-
-        int posIdx=0;
-        int negIdx=0;
-
-        for(int i=0;i<n;i++){
+        int[] ans = new int[n];
+        
+        int pos=0;
+        int neg=1;
+        for(int i=0;i<n; i++){
             if(nums[i]>=0){
-                pos[posIdx]=nums[i];
-                posIdx++;
-
+                
+                ans[pos]=nums[i];
+                
+                pos=pos+2;
             }
             else{
-                neg[negIdx]=nums[i];
-                negIdx++;
-
-            } 
+                ans[neg]=nums[i];
+                neg=neg+2;
+            }
         }
-
-        for(int j=0;j<n/2;j++){
-            nums[2*j]=pos[j];
-            nums[2*j+1]=neg[j];
-
-        }
-
-        return nums;
+        return ans;
 
 
     }
