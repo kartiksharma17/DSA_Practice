df1e9e63249330937bf3a24388de37a7d19853db
damnyoukartik <121182425+kartiksharma17@users.noreply.github.com>
2026-07-19 17:30:13 +0530

    Time: 0 ms (100%), Space: 43.9 MB (16.63%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,7 +2,7 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length; 
 
-        //transpose of matrix kiya
+        //transpose of matrix kiya: transpose diagonal k element same rehte hai (left-->right wale)  
         for(int i=0;i<n;i++){
 
             for(int j=i+1;j<n;j++){
