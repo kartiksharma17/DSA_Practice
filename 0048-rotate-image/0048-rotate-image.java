d8288eda3052e6db9a61545c246c57061fb8aae5
damnyoukartik <121182425+kartiksharma17@users.noreply.github.com>
2026-07-19 17:28:34 +0530

    Time: 0 ms (100%), Space: 43.6 MB (86.44%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length; 
+
+        //transpose of matrix kiya
+        for(int i=0;i<n;i++){
+
+            for(int j=i+1;j<n;j++){
+                
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+    
+        }
+
+
+        //last column swap kiya transpose matrix ka 
+        for(int i=0; i<n;i++){
+            int left=0;
+            int right=n-1;
+
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+
+                left++;
+                right--;
+            }
+        }
+        
+    }
+}
